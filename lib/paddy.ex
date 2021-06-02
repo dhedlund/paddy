729f966f47b26937197a68496e7f336a415d4f4a
Daniel Hedlund <daniel@digitree.org>
2021-06-02 14:21:22 -0700

    Initial commit

---
+++ lib/paddy.ex
@@ -0,0 +1,18 @@
+defmodule Paddy do
+  @moduledoc """
+  Documentation for `Paddy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Paddy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
